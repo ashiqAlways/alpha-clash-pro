b80b6c2b4e635e57af2614e6347549c185875d66
Ashik <ashikkhan757@gmail.coml>
2025-12-11 03:15:21 +0600

    added two js files

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,17 @@
+// function play(){
+//     // step-1: hide the home screen. to hide the screen add the class hidden to the home setion
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+   
+//     // show the playground
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
+
