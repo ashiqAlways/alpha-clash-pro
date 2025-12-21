e21bac955296df0b04a51d346bdc4676223932cd
Ashik <ashikkhan757@gmail.coml>
2025-12-21 13:15:48 +0600

    update some code

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,59 +1,51 @@
-function hideElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('hidden');
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
 
-function showElementById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('hidden')
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
 }
 
-
-function setBackroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+function setBackroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-400");
 }
 
-function removeBackgroundColorById(elementId){
-    const element = document.getElementById(elementId);
-    element.classList.remove('bg-orange-400');
+function removeBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
 }
 
-
-function getTextElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValueText = element.innerText;
-    const value = parseInt(elementValueText);
-    return value;
-
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
 }
 
-function setTextElementValueById(elementId, value){
-    const element = document.getElementById(elementId);
-    element.innerText = value;
+function setTextElementValueById(elementId, value) {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
 }
 
-
-function getElementTextById(elementId){
-    const element = document.getElementById(elementId);
-    const text = element.innerText;
-    return text;
+function getElementTextById(elementId) {
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
 }
 
+function getARandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
 
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
 
-function getARandomAlphabet(){
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
-    const alphabets = alphabetString.split('');
-
-    const randomNumber = Math.random() * 25;
-    const index = Math.round(randomNumber);
-
-    const alphabet = alphabets[index];
-    return alphabet;
+  const alphabet = alphabets[index];
+  return alphabet;
 }
-
-
 
 /*
 function getARandomAlphabet(){
@@ -71,5 +63,3 @@ function getARandomAlphabet(){
     return alphabet;
 }
     */
-
-
